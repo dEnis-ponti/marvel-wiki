237577befff5bed34e11bce61c1602096f7dfb19
dEnis-ponti <delavaifag@gmail.com>
2024-02-02 08:56:31 +0300

    lesson 175, additional routes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,6 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
-import {MainPage, ComicsPage} from "../pages";
+import {MainPage, ComicsPage, Page404, SingleComicPage} from "../pages";
 import AppHeader from '../appHeader/AppHeader';
 
 const App = () => {
@@ -16,6 +16,9 @@ const App = () => {
             <Route element={<MainPage />} path="/">
             </Route>
             <Route element={<ComicsPage />} path="/comics">
+            </Route>
+            <Route element={<SingleComicPage />} path="/comics/:comicId" />
+            <Route element={<Page404 />} path="*">
             </Route>
           </Routes>
         </main>
--- src/components/pages/ComicsPage.js
+++ src/components/pages/ComicsPage.js
@@ -2,6 +2,7 @@ import ComicsList from "../comicsList/ComicsList";
 import AppBanner from "../appBanner/AppBanner";
 
 const ComicsPage = () => {
+
   return (
     <>
       <AppBanner />
